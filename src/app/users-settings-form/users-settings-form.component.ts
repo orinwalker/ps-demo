f58fbea83809274494a19fd0d90ac2de5bdebfc0
Orin Walker <orin.walker@gmail.com>
2019-11-25 06:07:55 -0500

    Added UserSettings interface, two way data binding, and a copy of the form data

--- src/app/users-settings-form/users-settings-form.component.ts
+++ src/app/users-settings-form/users-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-users-settings-form',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-settings-form.component.css']
 })
 export class UsersSettingsFormComponent implements OnInit {
+
+  originalUserSettings: UserSettings = {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes...'
+  };
+
+  userSettings: UserSettings = {...this.originalUserSettings};
 
   constructor() { }
 
